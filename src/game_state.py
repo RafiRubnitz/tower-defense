4dcf87dbe157e6dc360ba14c7a9f5d53b19e9802
rafi rubnitz <rr0556750024@gal.com>
2026-04-14 01:56:47 +0300

    bugfix(game_state): add map selection and settings

--- src/game_state.py
+++ src/game_state.py
@@ -4,6 +4,8 @@ from enum import Enum
 class GameState(Enum):
     """Enum for different game states"""
     MAIN_MENU = "main_menu"
+    MAP_SELECTION = "map_selection"
+    SETTINGS = "settings"
     PLAYING = "playing"
     PAUSED = "paused"
     GAME_OVER = "game_over"
